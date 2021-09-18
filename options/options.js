c2536d315651a18bf9be362a98b0cf918081ccc4
Crowdbotics <team@crowdbotics.com>
2021-09-18 08:28:23 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "monogomis_30657Identifier",
+  url: "https://monogomis_30657Identifier.botics.co",
+  api: "https://monogomis_30657Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
